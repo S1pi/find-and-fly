70b57e4efac824e53c412040c1df34e1a6efd78a
Miika Sipilä <miikavsipila@gmail.com>
2025-02-11 15:30:40 +0200

    Button changes

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -7,25 +7,27 @@ const Header = () => {
   const [loggedIn, setLoggedIn] = useState<boolean>(false);
 
   return (
-    <div className='flex justify-center items-center w-full h-22 px-2 bg-primary relative'>
+    <div className='relative flex h-22 w-full items-center justify-center bg-primary px-2'>
       <img
         src='/img/FindAndFlyLogo.png'
         alt='Find&Fly Logo'
-        className='w- h-3/4 rounded-2xl absolute left-4'
+        className='w- absolute left-4 h-3/4 rounded-2xl'
       />
-      <h1 className='xl:text-h2 lg:text-h3 sm:text-h4 font-bold text-gray-800'>
+      <h1 className='text-h5 font-bold text-gray-800 sm:text-h4 lg:text-h3 xl:text-h2'>
         Explore, review and fly to your dream spots
       </h1>
       {loggedIn ? (
-        <BaseBtn className='absolute right-20'>Profile</BaseBtn>
+        <BaseBtn className='absolute right-20' evtHanlder={() => {}}>
+          Profile
+        </BaseBtn>
       ) : (
         <BaseBtn className='absolute right-26'>Login</BaseBtn>
       )}
-      <button className='rounded-full border-1 border-secondary p-1 absolute right-14'>
-        <MdOutlineLanguage className='text-secondary text-h1' />
+      <button className='absolute right-14 rounded-full border-1 border-secondary p-1'>
+        <MdOutlineLanguage className='text-h1 text-secondary' />
       </button>
-      <button className='rounded-full border-1 border-secondary p-1 absolute right-2'>
-        <FaStar className='text-secondary text-h1' />
+      <button className='absolute right-2 rounded-full border-1 border-secondary p-1'>
+        <FaStar className='text-h1 text-secondary' />
       </button>
     </div>
   );
--- frontend/src/components/buttons/BaseBtn.tsx
+++ frontend/src/components/buttons/BaseBtn.tsx
@@ -3,14 +3,15 @@ import {ReactNode} from 'react';
 type btnProps = {
   children: ReactNode;
   className?: string; // extra styles for the button
+  evtHanlder: () => void;
 };
 
-const BaseBtn = ({children, className}: btnProps) => {
+const BaseBtn = ({children, className, evtHanlder = () => {}}: btnProps) => {
   const baseStyles =
     'bg-gradient-to-r from-blue-btn via-55% via-blueg1 to-blueg2 text-white text-button font-medium py-2 px-4 rounded-4xl w-30';
 
   return (
-    <button className={`${baseStyles} ${className}`}>
+    <button className={`${baseStyles} ${className}`} onClick={evtHanlder}>
       <p>{children}</p>
     </button>
   );

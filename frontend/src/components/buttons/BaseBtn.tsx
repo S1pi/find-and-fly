ee5badcdb1c6e3e966126b34f42fa8f0ca53b458
S1pi <miikavsipila@gmail.com>
2025-03-14 16:13:52 +0200

    Some baseStyles added for button component

--- frontend/src/components/buttons/BaseBtn.tsx
+++ frontend/src/components/buttons/BaseBtn.tsx
@@ -8,7 +8,7 @@ type btnProps = {
 
 const BaseBtn = ({children, className, onClick = () => {}}: btnProps) => {
   const baseStyles =
-    'bg-gradient-to-r from-blue-btn via-55% via-blueg1 to-blueg2 text-primary text-button font-medium py-2 px-4 rounded-4xl w-30 cursor-pointer';
+    'bg-gradient-to-r from-blue-btn via-55% via-blueg1 to-blueg2 text-primary text-button font-medium py-2 px-4 rounded-4xl w-30 cursor-pointer drop-shadow-text hover:from-blueg1 hover:to-blueg2 hover:text-secondary';
 
   return (
     <button className={`${baseStyles} ${className}`} onClick={onClick}>
